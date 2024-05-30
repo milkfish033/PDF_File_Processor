08072c0ca0b96b58f554d586e72ec326a3efb3d0
Kerwin <141232455+milkfish033@users.noreply.github.com>
2024-05-30 15:02:34 +0800

    Updated 05/30/2024

---
+++ myproject/pdf_reader.py
@@ -0,0 +1,23 @@
+from llama_index.core import (SimpleDirectoryReader, download_loader, VectorStoreIndex)
+from llama_index.readers.file import PDFReader
+import doubao
+from multiprocessing import Process, freeze_support, set_start_method
+
+def generate(filepaths) -> str:
+  
+    #otherwise it there will multi-processing issues
+    freeze_support()
+    reader = SimpleDirectoryReader(input_files=filepaths,required_exts=[".pdf"],num_files_limit=1)
+  
+    document = reader.load_data(num_workers = 1)
+    #specify the number of files you want to load simultaneously
+    #*:note it couldn't be bigger than the number of your CPU
+  
+    #generate relative answer 
+    result = doubao.QA(document[0].get_text())
+  
+    return result
+
+
+
+
